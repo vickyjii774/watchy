12b433d20f5b119db69ff66598e513363f39df5e
bikash kattel <bikashkattel774@gmail.com>
2026-03-20 19:59:12 +0545

    Update database connection parameters

--- components/connect.php
+++ components/connect.php
@@ -1,13 +1,10 @@
 <?php
-
-$db_name = 'mysql:host=localhost;dbname=shop_db';
-$user_name = 'root';
-$user_password = '';
-
+$db_name = 'mysql:host=sql206.infinityfree.com;dbname=if0_41438495_watchy';
+$user_name = 'if0_41438495';
+$user_password = 'r6ju4wF10r';
 $conn = new PDO($db_name, $user_name, $user_password);
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $conn->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 $conn->exec("SET NAMES utf8mb4 COLLATE utf8mb4_general_ci");
 $conn->exec("SET CHARACTER SET utf8mb4");
-
 ?>
